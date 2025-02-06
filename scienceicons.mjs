b9d05a36e664cd9ca3eecd598a0aefa48ec284d5
Rowan Cockett <rowanc1@gmail.com>
2025-02-06 11:26:48 -0700

    Updates and linting

--- scienceicons.mjs
+++ scienceicons.mjs
@@ -1,47 +1,47 @@
 // node_modules/nanoid/index.js
-import { randomFillSync } from 'crypto'
-var POOL_SIZE_MULTIPLIER = 128
-var pool
-var poolOffset
+import { randomFillSync } from 'crypto';
+var POOL_SIZE_MULTIPLIER = 128;
+var pool;
+var poolOffset;
 var fillPool = (bytes) => {
   if (!pool || pool.length < bytes) {
-    pool = Buffer.allocUnsafe(bytes * POOL_SIZE_MULTIPLIER)
-    randomFillSync(pool)
-    poolOffset = 0
+    pool = Buffer.allocUnsafe(bytes * POOL_SIZE_MULTIPLIER);
+    randomFillSync(pool);
+    poolOffset = 0;
   } else if (poolOffset + bytes > pool.length) {
-    randomFillSync(pool)
-    poolOffset = 0
+    randomFillSync(pool);
+    poolOffset = 0;
   }
-  poolOffset += bytes
-}
+  poolOffset += bytes;
+};
 var random = (bytes) => {
-  fillPool((bytes -= 0))
-  return pool.subarray(poolOffset - bytes, poolOffset)
-}
+  fillPool((bytes -= 0));
+  return pool.subarray(poolOffset - bytes, poolOffset);
+};
 var customRandom = (alphabet, defaultSize, getRandom) => {
-  let mask = (2 << (31 - Math.clz32((alphabet.length - 1) | 1))) - 1
-  let step = Math.ceil((1.6 * mask * defaultSize) / alphabet.length)
+  let mask = (2 << (31 - Math.clz32((alphabet.length - 1) | 1))) - 1;
+  let step = Math.ceil((1.6 * mask * defaultSize) / alphabet.length);
   return (size = defaultSize) => {
-    let id = ''
+    let id = '';
     while (true) {
-      let bytes = getRandom(step)
-      let i = step
+      let bytes = getRandom(step);
+      let i = step;
       while (i--) {
-        id += alphabet[bytes[i] & mask] || ''
-        if (id.length === size) return id
+        id += alphabet[bytes[i] & mask] || '';
+        if (id.length === size) return id;
       }
     }
-  }
-}
-var customAlphabet = (alphabet, size = 21) => customRandom(alphabet, size, random)
+  };
+};
+var customAlphabet = (alphabet, size = 21) => customRandom(alphabet, size, random);
 
 // node_modules/myst-common/dist/utils.js
 function addMessageInfo(message, info) {
-  if (info === null || info === void 0 ? void 0 : info.note) message.note = info.note
-  if (info === null || info === void 0 ? void 0 : info.url) message.url = info.url
-  if (info === null || info === void 0 ? void 0 : info.ruleId) message.ruleId = info.ruleId
-  if (info === null || info === void 0 ? void 0 : info.fatal) message.fatal = true
-  return message
+  if (info === null || info === void 0 ? void 0 : info.note) message.note = info.note;
+  if (info === null || info === void 0 ? void 0 : info.url) message.url = info.url;
+  if (info === null || info === void 0 ? void 0 : info.ruleId) message.ruleId = info.ruleId;
+  if (info === null || info === void 0 ? void 0 : info.fatal) message.fatal = true;
+  return message;
 }
 function fileWarn(file, message, opts) {
   return addMessageInfo(
@@ -51,13 +51,13 @@ function fileWarn(file, message, opts) {
       opts === null || opts === void 0 ? void 0 : opts.source,
     ),
     opts,
-  )
+  );
 }
-var az = 'abcdefghijklmnopqrstuvwxyz'
-var alpha = az + az.toUpperCase()
-var numbers = '0123456789'
-var nanoidAZ = customAlphabet(alpha, 1)
-var nanoidAZ9 = customAlphabet(alpha + numbers, 9)
+var az = 'abcdefghijklmnopqrstuvwxyz';
+var alpha = az + az.toUpperCase();
+var numbers = '0123456789';
+var nanoidAZ = customAlphabet(alpha, 1);
+var nanoidAZ9 = customAlphabet(alpha + numbers, 9);
 
 // src/names.json
 var names_default = [
@@ -88,10 +88,10 @@ var names_default = [
   { name: 'twitter', componentName: 'TwitterIcon' },
   { name: 'website', componentName: 'WebsiteIcon' },
   { name: 'youtube', componentName: 'YoutubeIcon' },
-]
+];
 
 // src/scienceicons.ts
-var SUPPORTED_ICONS = names_default.map((icon) => icon.name)
+var SUPPORTED_ICONS = names_default.map((icon) => icon.name);
 var iconRole = {
   name: 'scienceicon',
   alias: ['scicon'],
@@ -102,12 +102,12 @@ var iconRole = {
   },
   run(data, vfile, ctx) {
     if (!SUPPORTED_ICONS.includes(data.body)) {
-      return fileWarn(vfile, `Unknown kind of scienceicon: ${data.body}`)
+      return fileWarn(vfile, `Unknown kind of scienceicon: ${data.body}`);
     }
-    return [{ type: 'scienceicon', value: data.body, kind: data.body }]
+    return [{ type: 'scienceicon', value: data.body, kind: data.body }];
   },
-}
-var plugin = { name: 'Science Icons by Curvenote', roles: [iconRole] }
-var scienceicons_default = plugin
-export { scienceicons_default as default }
+};
+var plugin = { name: 'Science Icons by Continuous Science Foundation', roles: [iconRole] };
+var scienceicons_default = plugin;
+export { scienceicons_default as default };
 //# sourceMappingURL=scienceicons.mjs.map
--- scripts/verify-names.js
+++ scripts/verify-names.js
@@ -1,23 +1,23 @@
-const fs = require('fs').promises
-const path = require('path')
+const fs = require('fs').promises;
+const path = require('path');
 
 const srcPaths = {
   mini: path.resolve(__dirname, '../src/20/solid/'),
   solid: path.resolve(__dirname, '../src/24/solid/'),
   outline: path.resolve(__dirname, '../src/24/outline/'),
-}
+};
 
 async function main() {
   let files = await Promise.all(
     Object.entries(srcPaths).map(async ([name, path]) => {
-      return { name, files: (await fs.readdir(path)).filter((file) => file.endsWith('.svg')) }
-    })
-  )
+      return { name, files: (await fs.readdir(path)).filter((file) => file.endsWith('.svg')) };
+    }),
+  );
 
-  let diffs = []
+  let diffs = [];
   for (let current of files) {
     for (let other of files) {
-      if (current === other) continue
+      if (current === other) continue;
 
       for (let file of current.files) {
         if (!other.files.includes(file)) {
@@ -25,16 +25,16 @@ async function main() {
             package: current.name,
             file: file,
             'Missing in?': other.name,
-          })
+          });
         }
       }
     }
   }
   if (diffs.length > 0) {
-    console.table(diffs)
+    console.table(diffs);
   } else {
-    console.log('All good!')
+    console.log('All good!');
   }
 }
 
-main()
+main();
